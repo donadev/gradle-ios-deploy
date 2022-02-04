5bc4dde6fdd5e66ae09996b28ac339759bf9bc3d
Luigi Donadel <luigi.donadel@gmail.com>
2022-02-04 13:01:29 +0100

    0.0.20

--- plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
 object PluginCoordinates {
     const val ID = "io.github.donadev.kmm.ios_deploy.plugin"
     const val GROUP = "io.github.donadev.kmm.ios_deploy"
-    const val VERSION = "0.0.19"
+    const val VERSION = "0.0.20"
     const val IMPLEMENTATION_CLASS = "io.github.donadev.kmm.ios_deploy.plugin.DeployPlugin"
 }
 
